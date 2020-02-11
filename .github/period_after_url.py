f929dbfe9c855bbe8e80e6e67834f9f84623842f
Sonny-MultiSafepay <sonny.danso@multisafepay.com>
2020-02-11 12:05:49 +0100

    INTGRTN-1367: Add Github Workflow - Period after URL

---
+++ .github/period_after_url.py
@@ -0,0 +1,20 @@
+import glob
+import os
+import re
+
+def searcher():
+    periodafterurl = False
+
+    for file in glob.glob(os.path.join('**/*.md'),recursive=True):
+        with open(file, 'r') as f:
+            content = f.read()
+            result = re.finditer('(\/*\)).$', content)
+            if result is not None:
+                for res in result:
+                    print('A period at the end of a URL is not allowed: {1}'.format(
+                        res.group(0), file))
+                    periodafterurl = True
+    return periodafterurl
+
+if searcher():
+    raise ValueError
