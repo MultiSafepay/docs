cf581480048adccf9a231272c09c3564b54b0fb1
Sarah-MultiSafepay <89071779+Sarah-MultiSafepay@users.noreply.github.com>
2021-10-26 09:53:09 +0200

    DOCS-2707: update urlchecker.py and add github action

--- .github/urlchecker.py
+++ .github/urlchecker.py
@@ -1,84 +1,25 @@
+import glob
+import os
 import re
-from pathlib import Path
-from operator import itemgetter
-import requests
 
-IGNORE_CODE = [200, 301, 302, 999]
+# This script checks links to markdown anchors in headers. If the link contains a / at the end, an error is raised.
 
-LINK_TEMP = 'HTTP Status: {0}, \t url:{1}, \t Document: {2}'
-PATH = '{0}/content'.format(Path(__file__).parent.parent)
-ROOT_URL = 'http://docs.multisafepay.com'
+regex = '(\/[\w\-\.]+)+\/#[\w\-\.]+[\/]|\/#[\w\-\.]+[\/]'
 
 
-def main():
-    status = False
-    path = PATH
-    root_url = ROOT_URL
+def searcher():
+    markdownlink = False
 
-    ## find links in markdown files
-    links = []
-    for path in walk(path):
-        for link in find_links(path):
-            if link[1].startswith('/'):
-                l = root_url + link[1]
-            elif link[1].startswith('#'):
-                continue
-            elif link[1].find('png') >= 0 or link[1].find('mailto') >= 0:
-                continue
-            else:
-                l = link[1]
+    for file in glob.glob(os.path.join('**/*.md'),recursive=True):
+        with open(file, 'r') as f:
+            content = f.read()
+            result = re.finditer(regex, content)
+            if result is not None:
+                for res in result:
+                    print('The following markdown URL: {0} in file {1} is incorrect. '
+                          'A \'/\' at the end of the URL is not allowed.'.format(res.group(0), file))
+                    markdownlink = True
+    return markdownlink
 
-            try:
-                r = requests.get(l, allow_redirects=True, timeout=10)
-                if r.status_code not in IGNORE_CODE:
-                    if r.status_code != 404 and not 'multisafepay.com' in l:
-                        continue
-                    links.append([str(r.status_code), str(l), str(path)])
-                else:
-                    continue
-            except:
-                if 'https://www.shopify.com/login?redirect' in l: # If it happens with more links, add function to check
-                    print('{0} has hit the rate limit for request.'.format(l))
-                else:
-                    links.append(['error', str(l), str(path)])
-
-    links = sorted(links, key=itemgetter(1))
-
-    if len(links) > 0:
-        status = True
-        for link in links:
-            print(link)
-    return status
-
-
-def walk(start):
-    paths = []
-    for filename in Path(start).glob('**/*.md'):
-        paths.append(filename)
-    return paths
-
-
-def get_content(path):
-    with open(path, 'r') as content_file:
-        return content_file.read()
-
-
-def find_links(path):
-    content = get_content(path)
-    name_regex = "[^]]+"
-    url_regex = "[^)]+"
-    markup_regex = '\[({0})]\(\s*({1})\s*\)'.format(name_regex, url_regex)
-
-    matches = []
-    for match in re.findall(markup_regex, content):
-        if match[1].startswith('<mailto:'):
-            continue
-        matches.append(match)
-
-    return matches
-
-
-if `name` == "__main__":
-    status = main()
-    if status:
-        raise ValueError
+if searcher():
+    raise ValueError
