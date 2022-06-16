51dcb349347aa97012ce8a9c2b4da06369e4ba11
Kris-MultiSafepay <kris.stallenberg@multisafepay.com>
2022-06-16 15:09:20 +0200

    DOCS-3383: Remove all Hugo-specific files

--- static/js/scroll-offset.js
+++
@@ -1,7 +0,0 @@
-const scrollOffset = (id, offset) => {
-    let target = document.getElementById(id);
-    if (target) {
-        let y = target.getBoundingClientRect().top + window.scrollY - offset;
-        window.scroll(0, y);
-    }
-}
