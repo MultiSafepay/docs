53f292479f822bf5eed201da22f389bed0feee54
Sarah-MultiSafepay <89071779+Sarah-MultiSafepay@users.noreply.github.com>
2022-07-12 13:01:44 +0200

    Update GitHub actions

--- .github/urlchecker.py
+++ .github/urlchecker.py
@@ -1,25 +1,38 @@
-import glob
-import os
 import re
+from pathlib import Path
 
 # This script checks links to markdown anchors in headers. If the link contains a / at the end, an error is raised.
+
+PATH = ['{0}/content'.format(Path(__file__).parent.parent),'{0}/api'.format(Path(__file__).parent.parent)]
 
 regex = '(\/[\w\-\.]+)+\/#[\w\-\.]+[\/]|#[\w\-\.]+[\/]'
 
 
-def searcher():
+def main():
     markdownlink = False
-
-    for file in glob.glob(os.path.join('**/*.md'),recursive=True):
-        with open(file, 'r') as f:
-            content = f.read()
-            result = re.finditer(regex, content)
-            if result is not None:
-                for res in result:
-                    print('The following markdown URL: {0} in file {1} is incorrect. '
-                          'A \'/\' at the end of the URL is not allowed.'.format(res.group(0), file))
-                    markdownlink = True
+    for path in PATH:
+      for file in walk(path):
+          with open(file, 'r') as f:
+              content = f.read()
+              result = re.finditer(regex, content)
+              if result is not None:
+                  for res in result:
+                      print('The following markdown URL: {0} in file {1} is incorrect. '
+                            'A \'/\' at the end of the URL is not allowed.'.format(res.group(0), file))
+                      markdownlink = True
     return markdownlink
 
-if searcher():
-    raise ValueError
+ignore_files = []
+
+def walk(start):
+    paths = []
+    for filename in Path(start).glob('**/*.md'):
+        if any(path in str(filename) for path in ignore_files):
+            continue
+        paths.append(filename)
+    return paths
+
+
+if __name__ == "__main__":
+    if main():
+        raise ValueError
