3fa05e65c09277c051d19ffc14fee341068ce182
Sarah-MultiSafepay <89071779+Sarah-MultiSafepay@users.noreply.github.com>
2022-08-15 15:38:59 +0200

    DOCS-3500: update external URLs workflow

--- .github/external-link-replace.py
+++ .github/external-link-replace.py
@@ -2,17 +2,17 @@ import re
 from pathlib import Path
 import os
 
-PATH = os.environ.get("PATHS")
+PATH = str.split(os.environ.get("DOCS_PATHS"))
 
 regex = re.compile(r'\[([^\[\]]*)\]\((https?:\/\/[\w\d./?=#&\+%-]+)\)')
 replacement = r'<a href="\2" target="_blank">\1</a> <i class="fa fa-external-link" style="font-size:12px;color:#8b929e"></i>'
 
 
 def main():
-    for path in PATH:
-        for file in walk(path):
-            print(file)
-            replace_urls(file)
+    print('List of files: {0}' .format(PATH))
+    for file in PATH:
+        print('Working on: {0}' .format(file))
+        replace_urls(file)
 
 
 ignore_files = []
@@ -26,16 +26,6 @@ def replace_urls(file_name):
         with open(file_name, 'w') as file:
             file.write(filedata)
             file.close()
-
-
-def walk(start):
-    paths = []
-    for filename in Path(start).glob('**/*.md'):
-        if any(path in str(filename) for path in ignore_files):
-            continue
-        paths.append(filename)
-    return paths
-
 
 if __name__ == "__main__":
     if main():
